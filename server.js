fb9029849e29e9935c840f31dae96b1a4b42a02e
jixin <coderj@foxmail.com>
2017-09-12 16:41:22 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    port : 8050,
+    workers: process.env.PRERENDER_NUM_WORKERS,
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    pageDoneCheckTimeout:1000,
+    resourceDownloadTimeout:15000,
+    waitAfterLastRequest:1000,
+    jsTimeout:15000,
+    jsCheckTimeout:1000,
+    evaluateJavascriptCheckTimeout:1000
 });
 
 
